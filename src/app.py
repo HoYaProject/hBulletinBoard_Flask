9cbb8bda12d72a49f364314dac35516efd978d7d
llHoYall <hoya128@gmail.com>
2021-01-27 17:50:42 +0900

    Applied Bootstrap4

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 import config
 from .models.common import db
 from .views import main_views, question_views, answer_views
@@ -19,6 +20,9 @@ migrate.init_app(app, db)
 app.register_blueprint(main_views.bp)
 app.register_blueprint(question_views.bp)
 app.register_blueprint(answer_views.bp)
+
+# Bootstrap
+Bootstrap(app)
 
 
 if __name__ == "__main__":
