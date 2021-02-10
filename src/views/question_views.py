ed74e80b6abb7e6762792ae50a53b64a46855f03
llHoYall <hoya128@gmail.com>
2021-02-10 17:14:35 +0900

    Added Pagination

--- src/views/question_views.py
+++ src/views/question_views.py
@@ -8,20 +8,26 @@ from ..forms import QuestionForm, AnswerForm
 bp = Blueprint("question", __name__, url_prefix="/question")
 
 
-@bp.route('/create/', methods=['GET', 'POST'])
+@bp.route("/create/", methods=["GET", "POST"])
 def create():
     form = QuestionForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        question = QuestionModel(subject=form.subject.data, content=form.content.data, created_date=datetime.now())
+    if request.method == "POST" and form.validate_on_submit():
+        question = QuestionModel(
+            subject=form.subject.data,
+            content=form.content.data,
+            created_date=datetime.now(),
+        )
         db.session.add(question)
         db.session.commit()
-        return redirect(url_for('main.index'))
-    return render_template('question/question_form.html', form=form)
+        return redirect(url_for("main.index"))
+    return render_template("question/question_form.html", form=form)
 
 
 @bp.route("/list/")
 def _list():
+    page = request.args.get("page", type=int, default=1)
     question_list = QuestionModel.query.order_by(QuestionModel.created_date.desc())
+    question_list = question_list.paginate(page, per_page=10)
     return render_template("question/question_list.html", question_list=question_list)
 
 
@@ -29,4 +35,6 @@ def _list():
 def detail(question_id):
     form = AnswerForm()
     question = QuestionModel.query.get_or_404(question_id)
-    return render_template("question/question_detail.html", question=question, form=form)
+    return render_template(
+        "question/question_detail.html", question=question, form=form
+    )
